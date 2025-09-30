66eb6f6912111b7cbb05b5e64cddfe71c584dd07
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-09-30 11:42:54 +0900

    feat: 애플리케이션 기본 세팅

---
+++ src/main/java/com/weeTalk/weeTalk/WeeTalkApplication.java
@@ -0,0 +1,13 @@
+package com.weeTalk.weeTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weeTalk/weeTalk/WeeTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weeTalk.weeTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
